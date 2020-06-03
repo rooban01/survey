087cfa957819a74fe9101a454b1c4cbad21e5b97
rooban01 <rooban01@users.noreply.github.com>
2020-06-04 00:28:57 +0200

    finished auth

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require ('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+ require ('./services/passort');
+ const authRoutes = require ('./routes/authRoutes');
+
+
+
+
+mongoose.connect(keys.mongoURI)
+.then(() => {
+    console.log("Connected to database!");
+  })
+    .catch(() => {
+    console.log("Connection failed!");
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-         hi: 'rooban'
-    });
-});
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+//we can do also    require ('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
