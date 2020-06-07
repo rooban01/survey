a7105001697fdeecc19dbf8d9f47083cdd239ea4
rooban01 <rooban01@users.noreply.github.com>
2020-06-07 15:07:56 +0200

    Add billing

---
+++ client/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+export const fetchUser = () => async dispatch => {
+
+ const res = await axios.get('/api/current_user');
+    dispatch ({ type: FETCH_USER, payload: res.data });
+  
+};
+
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe', token);
+    dispatch ({ type: FETCH_USER, payload: res.data });
+}
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+           <h1>
+              Survey
+           </h1>
+           Collect feedback
+        </div>
+    );
+};
+
+export default Landing;
